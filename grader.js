e71dc80cd6a4a0bdfa541ac1d9d2a4e7c77cd205
Chia-Chih Chen <ChiaChih.Chen@gmail.com>
2013-07-11 00:37:52 +0000

    grader.js with url option

--- grader.js
+++ grader.js
@@ -24,8 +24,12 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('sys');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://obscure-ravine-9648.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -35,6 +39,31 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var assertUrlExists = function(inUrl){
+    var instr = inUrl.toString();
+   /*
+    rest.get(instr).on('complete', function(result) {
+            if (result instanceof Error) {
+            sys.puts('Error: ' + result.message);
+            process.exit(1);
+            } else {
+            sys.puts(instr + ' is valid!');            
+            
+            fs.writeFile(tmpFile,result,function(err){
+                if(err) 
+                console.log(err);
+                else
+                console.log("saved!");                
+                });
+            }
+           
+            }
+            });
+    */     
+    return instr;
+}
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -51,9 +80,30 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
+        //console.log(ii);
     }
     return out;
 };
+
+var outfile = "hw3-part3.json";
+var checkUrlAddr = function(url,checksfile){    
+    
+    rest.get(url).on('complete',function(result) {           
+            //out = checkHtmlFile(result,checksfile);
+            var out = {};
+            $ = cheerio.load(result);
+            var checks = loadChecks(checksfile).sort();      
+            for(var ii in checks) {
+           var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+            }
+           var outJson = JSON.stringify(out, null, 4);
+            console.log(outJson);
+            fs.writeFileSync(outfile, outJson);  
+            });
+
+}
+
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -65,10 +115,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_addr>', 'Url to index.html', clone(assertUrlExists),URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    if(program.file != HTMLFILE_DEFAULT){
+        var checkJson = checkHtmlFile(program.file, program.checks);
+        var outJson = JSON.stringify(checkJson,null,4);
+    }
+    else
+        checkUrlAddr(program.url, program.checks);
+        //checkJson = checkHtmlFile(tmpFile,program.checks);
+/*
     var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    console.log(outJson);*/
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
